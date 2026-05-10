665b956ce6273488299e62f70663dbe34e092f8f
NikolayNikR <kolechka10.10@mail.ru>
2026-05-10 11:39:29 +0300

    создал экран с фоном и сделал движение коробля

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
+const int PLAYER_SPEED = 130;
+
 int main() {
 	sf::RenderWindow window(sf::VideoMode(700, 1000), "SpaceShooter");
 	sf::Texture texture;
 	texture.loadFromFile("C:\\Users\\ingin\\Documents\\С++\\Рычков Николай\\Assets\\SpaceWall.jpg");
 	sf::Sprite space;
 	space.setTexture(texture);
+
+	sf::RectangleShape player;
+	player.setSize(sf::Vector2f(100, 100));
+	player.setPosition(sf::Vector2f(window.getSize().x / 2, 800));
+	player.setOrigin(player.getSize().x/2, 0);
 	
 
 	sf::Clock clock;
@@ -20,11 +27,26 @@ int main() {
 		}
 		sf::Time delta_time = clock.restart();
 
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+			sf::Vector2f currentPos = player.getPosition();
+			if (player.getPosition().x < window.getSize().x - player.getSize().x / 2) {
+				player.setPosition(sf::Vector2f(currentPos.x + PLAYER_SPEED * delta_time.asSeconds(),currentPos.y));
+			}
+			
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+			sf::Vector2f currentPos = player.getPosition();
+			if (player.getPosition().x > 0 + player.getSize().x/2) {
+				player.setPosition(sf::Vector2f(currentPos.x - PLAYER_SPEED * delta_time.asSeconds(), currentPos.y));
+			}
+		}
+
 
 
 		
 		window.clear();
 		window.draw(space);
+		window.draw(player);
 		window.display();
 	}
 }
